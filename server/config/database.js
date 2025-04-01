16e3b6347365e562b568e618a4b2007fc0ea3990
wallisCodes <jwallis152@gmail.com>
2025-04-01 16:24:29 +0100

    Enable dual database config

--- server/config/database.js
+++ server/config/database.js
@@ -2,22 +2,31 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log(`Current time inside databse.js: ${new Date()}`);
-// Debugging env variables
-// console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-// console.log('typeof DB_PASSWORD:', typeof process.env.DB_PASSWORD);
+const isProduction = process.env.NODE_ENV === 'production';
+isProduction && console.log(`Current time inside databse.js: ${new Date()}`);
 
-// Database connection details
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST || 'localhost',
+// Create sequelize instance based on node environment
+const sequelize = isProduction
+    ? new Sequelize(process.env.DATABASE_URL, {
         dialect: 'postgres',
-        logging: false
-        // logging: console.log
-    }
+        logging: false, 
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Required for AWS RDS
+            }
+        }
+    })
+    : new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'postgres',
+            logging: false
+            // logging: console.log
+        }
 );
 
 export default sequelize;
